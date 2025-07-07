c4b720798b3a25e5091ba7493fb1efd0cddce50a
Idayat Sanni <idayatsanni@gmail.com>
2025-07-07 09:15:29 -0400

    Add Register component with lnk to login

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,6 @@ import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <Router>
       <div className='max-w-4xl mx-auto p-4'>
